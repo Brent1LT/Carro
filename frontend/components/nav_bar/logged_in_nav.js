0b37e7ea70b8ac0d66b3f29d68815ea349d4d809
Brent Bumann <brent.bumann@aol.com>
2019-04-09 13:46:36 -0700

    Adjust nav links in nav bar

--- frontend/components/nav_bar/logged_in_nav.js
+++ frontend/components/nav_bar/logged_in_nav.js
@@ -5,13 +5,21 @@ import {Link} from 'react-router-dom';
 const LoggedInNav = () => {
   return (
     <div className="nav-user-profile">
-      <Link to={`/listings/new`} className='nav-user-links'>List Your Car</Link>
-      <a href="" className="nav-user-links">Trips</a>
-      <a href="" className="nav-user-links">Messages</a>
-      <a href="" className="nav-user-links">Learn More</a>
+      <Link to={`/listings/new`} className="nav-user-links">
+        List Your Car
+      </Link>
+      <a href="" className="nav-user-links">
+        Trips
+      </a>
+      <a href="" className="nav-user-links">
+        Messages
+      </a>
+      <a href="https://github.com/Brent1LT/Carro" target='_blank' className="nav-user-links">
+        Learn More
+      </a>
       <ProfileDropdownContainer />
     </div>
-  )
+  );
 }
 
 export default LoggedInNav;
--- frontend/components/nav_bar/logged_out_nav.js
+++ frontend/components/nav_bar/logged_out_nav.js
@@ -2,30 +2,38 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const LoggedOutNav = ({handleResponse, openModal}) =>{
-  return(
+  return (
     <div className="nav-bar-links">
-      <Link className='nav-link' to="/listings/new" >List Your Car</Link>
-      <label className="nav-link" >Learn More</label>
+      <Link className="nav-link" to="/listings/new">
+        List Your Car
+      </Link>
+      <a href="https://github.com/Brent1LT/Carro" target='_blank' className="nav-link">
+        Learn More
+      </a>
 
       <a
-        className='nav-link'
-        href=''
-        onClick={(e) => {
+        className="nav-link"
+        href=""
+        onClick={e => {
           handleResponse(e);
-          openModal("Log in")
+          openModal("Log in");
         }}
-      >Login</a>
+      >
+        Login
+      </a>
 
-        <a
-          className='nav-link'
-          href=''
-          onClick={(e) => {
-            handleResponse(e);
-            openModal("Sign up")
-          }}
-        >Signup</a>
-    </div >
-  )
+      <a
+        className="nav-link"
+        href=""
+        onClick={e => {
+          handleResponse(e);
+          openModal("Sign up");
+        }}
+      >
+        Signup
+      </a>
+    </div>
+  );
 }
 
 export default LoggedOutNav;
