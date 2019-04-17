971d3179e9e133a74cf0cf2729c82a2dd98f1350
Brent Bumann <brent.bumann@aol.com>
2019-04-16 23:16:43 -0700

    Can post a booking from frontend

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,4 +1,18 @@
 class Api::BookingsController < ApplicationController
+  MONTHS = {
+    Jan: 1,
+    Feb: 2,
+    Mar: 3,
+    Apr: 4,
+    May: 5,
+    Jun: 6,
+    Jul: 7,
+    Aug: 8,
+    Sep: 9,
+    Oct: 10,
+    Nov: 11,
+    Dec: 12
+  }
 
   def index
     @bookings = Booking.all
@@ -6,19 +20,21 @@ class Api::BookingsController < ApplicationController
 
   def create
     # will probably need to fix once react/ajax is sending down info
-    debugger
-    start_dates = booking_params[:start_date].split(',')
-    end_dates = booking_params[:end_date].split(',')
-    start_dates = start_dates.map {|el| el.to_i}
-    end_dates = end_dates.map {|el| el.to_i}
-    start_date = DateTime.new(*start_dates)
-    end_date = DateTime.new(*end_dates)
+    start_dates = booking_params[:start_date].split(' ')
+    end_dates = booking_params[:end_date].split(' ')
+    start_month = MONTHS[start_dates[1].to_sym]
+    end_month = MONTHS[end_dates[1].to_sym]
+    start_dates = start_dates[2..3].map {|el| el.to_i}
+    end_dates = end_dates[2..3].map {|el| el.to_i}
+    start_date = DateTime.new(start_dates[1], start_month, start_dates[0])
+    end_date = DateTime.new(end_dates[1], end_month, end_dates[0])
     @booking = Booking.new(start_time: start_date, end_time: end_date)
     if(!@booking.is_valid?(params[:listing_id]))
       render json: "Overlap in booking schedule"
     else
-      @booking.user_id = 1
+      @booking.user_id = current_user.id
       @booking.listing_id = params[:listing_id].to_i
+      debugger
       if(@booking.save)
         # might need to change in jbuilder to have user id on outside of booking state
         render :create
